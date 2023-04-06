f995d8792a0d3e870f814f5473646b9dc3bdc08d
Nikita-Buria <m_buria@fanshaweonline.ca>
2023-04-06 02:33:59 -0400

    added js code

--- js/main.js
+++ js/main.js
@@ -1,69 +1,86 @@
-const albumCovers = document.querySelectorAll(".box img"),
-      theAudioEl = document.querySelector('audio'),
-      playButton = document.querySelector('#playButton'),
-      pauseButton = document.querySelector('#pauseButton'),
-      rewindButton = document.querySelector('#rewindButton'),
-      volSlider = document.querySelector('#volumeControl');
+document.addEventListener('DOMContentLoaded', function() {
+    const soundBoxes = document.querySelectorAll('.box');
+    const playingBoxes = document.querySelectorAll('.dropBox');
+    const allBoxes = document.querySelectorAll('.box, .dropBox');
+    const albumCovers = document.querySelectorAll(".box img");
+    const theAudioEl = document.querySelector('audio');
+    const playButton = document.querySelector('#playButton');
+    const pauseButton = document.querySelector('#pauseButton');
+    const rewindButton = document.querySelector('#rewindButton');
+    const volSlider = document.querySelector('#volumeControl');
 
-const soundBoxes = document.querySelectorAll(".box"),
-      dropBoxes = document.querySelectorAll(".dropBox");
+    allBoxes.forEach(box => {
+        box.addEventListener('dragstart', handleDragStart);
+    });
 
-let draggedElement = null;
+    allBoxes.forEach(box => {
+        box.addEventListener('dragover', handleDragOver);
+        box.addEventListener('drop', handleDrop);
+    });
 
-function loadAudio() {
-    let currentSrc = `audio/${this.dataset.trackref}.mp3`;
+    let draggedElement = null;
 
-    theAudioEl.src = currentSrc;    
-
-    theAudioEl.load();
-
-    playAudio();
-}
-
-function playAudio() { 
-    theAudioEl.play(); 
-}
-
-function restartAudio() { 
-    theAudioEl.currentTime = 0; 
-    playAudio(); 
-}
-
-function pauseAudio() { theAudioEl.pause(); }
-
-function setVolume() {
-    theAudioEl.volume = (this.value/100); 
-}
-
-function handleDragStart(e) {
-    draggedElement = this;
-    e.dataTransfer.setData('text/plain', '');
-}
-
-function handleDragOver(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'move';
-}
-
-function handleDrop(e) {
-    e.preventDefault();
-
-    if (draggedElement) {
-        this.appendChild(draggedElement);
-        this.querySelector('.track-ref').style.display = 'none';
+    function handleDragStart(e) {
+        draggedElement = e.target;
+        e.dataTransfer.effectAllowed = 'move';
     }
-}
 
-albumCovers.forEach(cover => cover.addEventListener('click', loadAudio));
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        e.dataTransfer.dropEffect = 'move';
+        return false;
+    }
 
-playButton.addEventListener('click', playAudio);
-rewindButton.addEventListener('click', restartAudio);
-pauseButton.addEventListener('click', pauseAudio);
+    function handleDrop(e) {
+        e.preventDefault();
 
-volSlider.addEventListener('change', setVolume);
+        if (draggedElement) {
+            // Clone the image from the sound box
+            const clonedImage = draggedElement.cloneNode(true);
 
-soundBoxes.forEach(soundBox => soundBox.addEventListener('dragstart', handleDragStart));
-soundBoxes.forEach(soundBox => soundBox.setAttribute('draggable', 'true'));
+            // Hide the playing box's original image
+            const trackRef = this.querySelector('.track-ref');
+            if (trackRef) {
+                trackRef.style.display = 'none';
+            }
 
-dropBoxes.forEach(dropBox => dropBox.addEventListener('dragover', handleDragOver));
-dropBoxes.forEach(dropBox => dropBox.addEventListener('drop', handleDrop));
+            // Append the cloned image to the playing box
+            this.appendChild(clonedImage);
+
+            // Hide the original image in the sound box
+            draggedElement.style.display = 'none';
+
+            // Show the original image when moving back to a clear div
+            if (this.classList.contains('box')) {
+                draggedElement.style.display = 'block';
+            }
+
+            // Remove the dragged image from the previous box
+            draggedElement.parentNode.removeChild(draggedElement);
+
+            // Make the cloned image draggable
+            clonedImage.draggable = true;
+            clonedImage.addEventListener('dragstart', handleDragStart);
+
+            // Hide the track icon in the playing box when dropping a sound-box image
+            if (this.classList.contains('dropBox') && trackRef) {
+                trackRef.style.display = 'none';
+            }
+
+            // Show the track icon back when moving the sound-box image back to the sound-box div
+            if (this.classList.contains('box')) {
+                const originalPlayingBox = Array.from(playingBoxes).find(box => box.contains(draggedElement));
+                if (originalPlayingBox) {
+                    const originalTrackRef = originalPlayingBox.querySelector('.track-ref');
+                    if (originalTrackRef) {
+                        originalTrackRef.style.display = 'block';
+                    }
+                }
+            }
+        }
+    }
+
+    // Add your event listeners for play, pause, rewind, and volume control here
+});
